66c07e0cc29f45124ed2fcb007fbb57af6e7e3d1
Nick Morgan <skilldrick@gmail.com>
2016-09-09 21:20:06 -0700

    Add Footer.js

---
+++ app/Footer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import logo from 'sine/logo';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <a
+        href="https://github.com/skilldrick/sine"
+        id="logo"
+        style={{
+          display: "block",
+          marginTop: "150px",
+          position: "relative"
+        }}
+      >
+        <p
+          style={{
+            fontFamily: "sans-serif",
+            verticalAlign: "middle",
+            position: "absolute",
+            color: "black",
+            left: "60px"
+          }}
+        >
+          Powered by Sine
+        </p>
+      </a>
+    )
+  }
+
+  componentDidMount() {
+    logo(document.getElementById("logo"), 50);
+  }
+}
+
